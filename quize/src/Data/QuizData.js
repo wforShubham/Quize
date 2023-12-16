fd3e3c9fb3444cf63445bd761f7f41d3517280ed
Shubham Vishwakarma <94854000+wforShubham@users.noreply.github.com>
2023-12-16 15:42:19 +0530

    Add files via upload

---
+++ quize/src/Data/QuizData.js
@@ -0,0 +1,98 @@
+export const QuizData = [
+    {
+        question: "What is the primary cause of earthquakes?",
+        options: [
+            "Volcanic eruptions",
+            "Tectonic plate movement"
+        ],
+        answer:2
+    },
+    {
+        question: "How can you protect yourself during an earthquake indoors?",
+        options: [
+        "Stand near a window",
+        "Drop, cover, and hold on"
+        ],
+        answer:2
+    },
+    {
+    question: "What is the primary cause of earthquakes?",
+    options: [
+        "Volcanic eruptions",
+        "Tectonic plate movement"
+    ],
+    answer:2
+    },
+    {
+    question: "How can you protect yourself during an earthquake indoors?",
+    options: [
+        "Stand near a window",
+        "Drop, cover, and hold on"
+    ],
+    answer:2
+    },
+    {
+    question: "What should you do if you are caught in a flash flood while driving?",
+    options: [
+        "Drive through it as quickly as possible", 
+        "Turn around and find another route"
+    ],
+    answer:2
+    },
+    {
+    question: "What are the essential items to include in an emergency preparedness kit?",
+    options: [
+        "Candles and matches",
+        "Water, food, and first aid supplies"
+    ],
+    answer:2
+    },
+    {
+    question: "How can you reduce the risk of a house fire?",
+    options: [
+        "Leave candles unattended",
+        "Install smoke detectors"
+    ],
+    answer:2
+    },
+    {
+    question: "What should you do if you encounter a tornado while in a vehicle?",
+    options: [
+        "Stay in the car",
+        "Get out and seek shelter in a low-lying area"
+    ],
+    answer:2
+    },
+    {
+    question: "What is the 'Stop, Drop, and Roll' technique used for?",
+    options: [
+        "Climbing mountains",
+        "Extinguishing clothing fires"
+    ],
+    answer:2
+    },
+    {
+    question: "How can you prepare for a hurricane?",
+    options: [
+        "Open all windows to equalize pressure",
+        "Secure outdoor objects and stock up on supplies"
+    ],
+    answer:2
+    },
+    {
+    question: "What is the difference between a hurricane and a typhoon?",
+    options: [
+        "Nothing, they are the same",
+        "The region in which they occur"
+    ],
+    answer:2
+    },
+    {
+    question: "What is the primary cause of wildfires?",
+    options: [
+        "Lightning strikes",
+        "Human activities and dry conditions"
+    ],
+    answer:2
+    },
+];
